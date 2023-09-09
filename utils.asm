0ff330c71979e2763b31878053968f16e300a8b3
turdshover <144286932+turdshover@users.noreply.github.com>
2023-09-09 11:28:30 -0400

    OffByOneAgain™

--- utils.asm
+++ utils.asm
@@ -3,20 +3,22 @@
 #import "equ.asm"
 
 clear_screen: {
-        ldx #$f9
+        ldx #$c7
 
     loop:
         lda CLR_CHAR
         sta SCREEN_RAM, x
-        sta SCREEN_RAM+$00fa, x
-        sta SCREEN_RAM+$01f4, x
-        sta SCREEN_RAM+$02ee, x
+        sta SCREEN_RAM+$00c8, x
+        sta SCREEN_RAM+$0190, x
+        sta SCREEN_RAM+$0258, x
+        sta SCREEN_RAM+$0320, x
 
         lda CLR_COL
         sta COLOR_RAM, x
-        sta COLOR_RAM+$00fa, x
-        sta COLOR_RAM+$01f4, x
-        sta COLOR_RAM+$02ee, x
+        sta COLOR_RAM+$00c8, x
+        sta COLOR_RAM+$0190, x
+        sta COLOR_RAM+$0258, x
+        sta COLOR_RAM+$0320, x
 
         dex
         cpx #$ff
